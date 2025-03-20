12dc6188d9ee39a78f53db2aa406363f06e38e7a
Inioluwa1 <adesanyanifesimi@gmail.com>
2025-03-20 09:48:17 +0100

    Mobile Responsiveness and pagination added a bit

--- src/Components/Testimony.jsx
+++ src/Components/Testimony.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Testimony.css"
 import Testimonies from "../Components/Testimonies"
 import TestimonyCard from './TestimonyCard'
 
 export default function Testimony() {
+  const [selected, setSelected] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSelected((prevSelected) => 
+        (prevSelected + 1) % Testimonies.length
+      )
+    }, 3000)
+
+    return () => clearInterval(interval);
+    
+  }, [])
+  
   return (
     <div className='TestimonyContainer'>
       <h1> What they've said </h1>
+
       <div className='IndTestimonyContainer'>
         {Testimonies.map((indTestimony, id)=>(
           <TestimonyCard 
@@ -17,7 +31,31 @@ export default function Testimony() {
           />
         ))}
       </div> 
-      <button> Get Started </button>
+
+      <div className='MobileIndTestimonyContainer'>
+        {Testimonies.map((indTestimony, index)=>(
+            <div 
+              key={indTestimony.id}
+              className={`MobileSlider ${index === selected? "active" : ""}`}
+            >
+              <img src={indTestimony.Image} alt={`Slide ${index + 1}`} />
+              <h1> {indTestimony.Name} </h1>
+              <p> "{indTestimony.About}" </p> 
+            </div>
+          ))}
+
+          <div className='Dotcontainer'>
+            {Testimonies.map((_, index) => (
+              <span
+                key={index}
+                onClick={() => setSelected(index)}
+              >
+                  Hey
+              </span>
+            ))}
+          </div>
+      </div>
+      <button> Get Started </button> 
     </div>
   )
 }
